be72ea88ab51de760c61b0b484f38c8e57e7ce48
twoics <second_vozhzhov@mail.ru>
2023-01-23 16:56:17 +0700

    Define failure code

--- src/timer/timer.c
+++ src/timer/timer.c
@@ -2,13 +2,14 @@
 #include "time.h"
 
 #define PERCENT_IN_SHARE 100
+#define FAILURE_CODE (-1)
 
 int percent_passed_time(time_t current_time, time_t start_time, time_t end_time) {
     double interval = (double) (end_time - start_time);
     double passed = (double) (current_time - start_time);
 
     if (interval == 0) {
-        return -1;
+        return FAILURE_CODE;
     }
 
     double share_passed = passed / interval;
