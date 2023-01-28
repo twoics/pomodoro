c98ba1d7814493f788d6ccd8a1b84d28a1a4c881
twoics <second_vozhzhov@mail.ru>
2023-01-28 15:04:06 +0700

    Change exception printf style

--- src/exceptions/exceptions.c
+++ src/exceptions/exceptions.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 #define EXIT_CODE (-1)
-#define SEPARATOR "-----------"
+#define SEPARATOR " -------------"
 
 void fatal_exit(char* message, const char* func_name, int line_number) {
     fprintf(stderr,
-            "%s\nFATAL ERROR\n%s\nFUNC: %s\nLINE: %d\nMESSAGE: %s",
+            "%s\n| FATAL ERROR |\n%s\nFUNC: %s\nLINE: %d\nMESSAGE: %s",
             SEPARATOR,
             SEPARATOR,
             func_name,
