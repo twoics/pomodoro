6a83d722df250db8ce46f3576c0423da9273d56e
twoics <second_vozhzhov@mail.ru>
2023-01-30 19:19:38 +0700

    Add progress_bar_settings

--- src/progress_bar/progress.c
+++ src/progress_bar/progress.c
@@ -1,19 +1,20 @@
 #include <malloc.h>
 #include "progress.h"
-#include "../bar/bar.h"
 
-void draw_progress(int percentage, struct bar_settings settings) {
-    const char** bar = build(percentage, settings);
+void draw_progress(int percentage, struct progress_bar_settings settings) {
+    const char** bar = build(percentage, settings.bar_setting);
 
-    int total_bar_len = settings.bar_len;
-    if (settings.left_border != NULL) {
+    struct bar_settings bar_setting = settings.bar_setting;
+
+    int total_bar_len = bar_setting.bar_len;
+    if (bar_setting.left_border != NULL) {
         total_bar_len++;
     }
-    if (settings.right_border != NULL) {
+    if (bar_setting.right_border != NULL) {
         total_bar_len++;
     }
 
-    for (int i = 0; i < settings.bar_len + total_bar_len; ++i) {
+    for (int i = 0; i < bar_setting.bar_len + total_bar_len; ++i) {
         printf("%s", bar[i]);
     }
 
--- src/progress_bar/progress.h
+++ src/progress_bar/progress.h
@@ -1,15 +1,12 @@
 #ifndef POMODORO_PROGRESS_H
 #define POMODORO_PROGRESS_H
 
-struct bar_settings {
-    const int bar_len;
-    const char* const left_border;
-    const char* const right_border;
-    const char* const fill;
-    const char* const empty;
-    const char* const current;
+#include "../bar/bar.h"
+
+struct progress_bar_settings {
+    struct bar_settings bar_setting;
 };
 
-void draw_progress(int percentage, struct bar_settings settings);
+void draw_progress(int percentage, struct progress_bar_settings settings);
 
 #endif //POMODORO_PROGRESS_H
