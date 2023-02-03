002c3771e45b571d0c6d0a8dca62cecc8e4c0cda
twoics <second_vozhzhov@mail.ru>
2023-02-03 20:16:38 +0700

    Fix bar output

--- src/session_bar/session_bar.c
+++ src/session_bar/session_bar.c
@@ -5,7 +5,15 @@ void draw_session_bar(int percentage, struct session_bar_setting settings) {
     const char** bar = build(percentage, settings.bar_settings);
     struct bar_settings bar_setting = settings.bar_settings;
 
-    for (int i = 0; i < bar_setting.bar_len; ++i) {
+    int total_bar_len = bar_setting.bar_len;
+    if (bar_setting.left_border != NULL) {
+        total_bar_len++;
+    }
+    if (bar_setting.right_border != NULL) {
+        total_bar_len++;
+    }
+
+    for (int i = 0; i < total_bar_len; ++i) {
         printf("%s", bar[i]);
     }
     printf("\n");
